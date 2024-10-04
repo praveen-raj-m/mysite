a96edc4d04490feb70ed5ddf115250d879648ccf
Praveen Raj Mohanraj <75660847+praveen-raj-m@users.noreply.github.com>
2024-10-04 16:56:26 -0700

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 import logo from "../assets/mylogo.png";
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false); // State to manage dropdown visibility
+  const [isOpen, setIsOpen] = useState(false); 
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen); // Toggle the dropdown menu
+    setIsOpen(!isOpen); 
   };
 
   return (
     <nav className="mb-15 flex flex-col items-center py-6 relative">
       <div className="flex justify-between w-full items-center">
-        {/* Logo on the Left */}
+     
         <div className="flex flex-shrink-0 items-center">
           <a href="/">
             <img
@@ -23,7 +23,7 @@ const NavBar = () => {
           </a>
         </div>
         
-        {/* Navigation Links for Medium and Larger Screens on the Right */}
+    
         <div className="hidden md:flex m-8 gap-4 text-4xl">
           <a href="#home" className="text-lg px-2 py-2 text-white">
             Home
@@ -39,45 +39,45 @@ const NavBar = () => {
           </a>
         </div>
 
-        {/* Toggle Button for Small Screens */}
+       
         <div className="md:hidden">
           <button
             onClick={toggleMenu}
             className="text-white text-4xl focus:outline-none"
           >
-            {isOpen ? "✖" : "☰"} {/* Change icon based on menu state */}
+            {isOpen ? "☰" : "☰"} 
           </button>
         </div>
       </div>
 
-      {/* Dropdown Section for Small Screens */}
+    
       {isOpen && (
         <div className="flex flex-col md:hidden bg-gray-800 rounded-md shadow-lg w-full mt-2">
           <a
             href="#home"
             className="text-lg px-4 py-2 text-white hover:bg-gray-700"
-            onClick={() => setIsOpen(false)} // Close menu on link click
+            onClick={() => setIsOpen(false)} 
           >
             Home
           </a>
           <a
             href="#experiences"
             className="text-lg px-4 py-2 text-white hover:bg-gray-700"
-            onClick={() => setIsOpen(false)} // Close menu on link click
+            onClick={() => setIsOpen(false)} 
           >
             Experiences
           </a>
           <a
             href="#projects"
             className="text-lg px-4 py-2 text-white hover:bg-gray-700"
-            onClick={() => setIsOpen(false)} // Close menu on link click
+            onClick={() => setIsOpen(false)} 
           >
             Projects
           </a>
           <a
             href="#contact"
             className="text-lg px-4 py-2 text-white hover:bg-gray-700"
-            onClick={() => setIsOpen(false)} // Close menu on link click
+            onClick={() => setIsOpen(false)} 
           >
             Contact
           </a>
