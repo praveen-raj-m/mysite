e0c596471924833886f0603dce943245bc54f9ef
Praveen Raj Mohanraj <75660847+praveen-raj-m@users.noreply.github.com>
2024-10-04 20:41:58 -0700

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,9 +8,9 @@ const Experience = () => {
     <section id="experiences">
       <div className="border-b border-neutral-900 pb-4">
         <motion.h1
-          whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: -100 }}
-          transition={{ duration: 0.5 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
           className="my-20 text-center text-5xl"
         >
           Experience
@@ -20,14 +20,14 @@ const Experience = () => {
             <motion.div 
               key={index} 
               className="mb-8 p-6 rounded-lg shadow-lg bg-neutral-900"
-              whileInView={{ opacity: 1, y: 0 }}
               initial={{ opacity: 0, y: 50 }}
-              transition={{ duration: 0.5, delay:  0.5 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
             >
               <div className="flex flex-wrap items-center">
                 <motion.div
-                  whileInView={{ opacity: 1, x: 0 }}
                   initial={{ opacity: 0, x: -100 }}
+                  animate={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.5 }}
                   className="w-full lg:w-2/5 flex flex-wrap lg:justify-center"
                 >
@@ -36,8 +36,8 @@ const Experience = () => {
                   </p>
                 </motion.div>
                 <motion.div
-                  whileInView={{ opacity: 1, x: 0 }}
                   initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.5 }}
                   className="w-full max-w-xl lg:w-3/5 flex flex-wrap lg:justify-left"
                 >
