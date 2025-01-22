7bc02d0cb06ab41c051d9a9cfdb599acf8264fea
Praveen Raj <pravnrjm@gmail.com>
2025-01-21 20:40:12 -0700

    minor changes

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -99,7 +99,7 @@ export default function Stack({
     >
       {cards.map((card, index) => {
         const randomRotate = randomRotation
-          ? Math.random() * 10 - 5 
+          ? Math.random() * 11 -1
           : 0;
 
         return (
