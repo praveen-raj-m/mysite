8d5e2e528662ddcde79bb8432f9efac31780d96d
Praveen Raj <pravnrjm@gmail.com>
2025-01-23 22:10:06 -0700

    minor changes

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -49,14 +49,17 @@ export default function Stack({
       { id: 1, img: "myimages/image1.jpeg" },
       { id: 2, img: "myimages/image5.jpeg" },
       { id: 3, img: "myimages/image4.jpeg" },
-      { id: 4, img: "myimages/image7.jpeg" },
-      { id: 5, img: "myimages/image8.jpeg" },
-      { id: 6, img: "myimages/image9.jpeg" },
+      { id: 4, img: "myimages/image11.jpeg" },
+      { id: 5, img: "myimages/image7.jpeg" },
+      { id: 6, img: "myimages/image8.jpeg" },
       { id: 7, img: "myimages/image10.jpeg" },
-      { id: 8, img: "myimages/image11.jpeg" },
-      { id: 9, img: "myimages/image2.jpeg" },
-      ];
-
+      { id: 8, img: "myimages/image13.jpeg" },
+      { id: 11, img: "myimages/image2.jpeg" },
+      { id: 10, img: "myimages/image14.jpeg" },
+      { id: 9, img: "myimages/image12.jpeg" },
+    
+    ];
+    
   const [cards, setCards] = useState(initialCards);
   const allImages = [
     "myimages/image1.jpeg",
@@ -64,10 +67,12 @@ export default function Stack({
     "myimages/image4.jpeg",
     "myimages/image7.jpeg",
     "myimages/image8.jpeg",
-    "myimages/image9.jpeg",
     "myimages/image10.jpeg",
     "myimages/image11.jpeg",
+    "myimages/image13.jpeg",
     "myimages/image2.jpeg",
+    "myimages/image14.jpeg",
+    "myimages/image12.jpeg",
   ];
 
   const sendToBack = (id) => {
